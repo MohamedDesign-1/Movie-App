e0b6e9835ebdbf36ce79af330d4c1b850b18c0ae
m7med <m7medyt2017@gmail.com>
2024-08-17 17:11:31 +0300

    add navbar and setup colors

---
+++ lib/screens/browse_screen/browse_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BrowseScreen extends StatelessWidget {
+  static const String routeName = "BrowseScreen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
---
+++ lib/screens/watchlist_screen/watch_list_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WatchListScreen extends StatelessWidget {
+  static const String routeName = "WatchListScreen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
