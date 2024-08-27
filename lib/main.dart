9d35c7ada4dddeb3e65cf61c1970f78ab85fe449
ahmed-elshafey <ahmedelshafee861@gmail.com>
2024-08-27 17:41:09 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+//import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:movieapp/screens/browse_screen/browse_screen.dart';
 import 'package:movieapp/screens/home_screen/home_screen.dart';
+import 'package:movieapp/screens/movie_details_screen/movie_details_screen.dart';
 import 'package:movieapp/style/theme_app.dart';
-import 'package:movieapp/widgets/bottom_nav_bar.dart'; // تأكد من استيراد الـ BottomNavBar
+import 'package:movieapp/widgets/bottom_nav_bar.dart';
 
-void main() {
+import 'models/movie.dart'; // تأكد من استيراد الـ BottomNavBar
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(MovieAdapter());
+  await Hive.openBox<Movie>('watch_list_movies');
+
   runApp(MovieApp());
 }
 
@@ -18,6 +28,7 @@ class MovieApp extends StatelessWidget {
         MainPage.routeName : (context) => MainPage(),
         HomeScreen.routeName : (context) => HomeScreen(),
         BrowseScreen.routeName : (context) => BrowseScreen(),
+        MovieDetailsScreen.routeName: (context) => MovieDetailsScreen(),
       },
       theme: AppTheme.mainTheme,
     );
